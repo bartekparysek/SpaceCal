e9c33cb83461f23d83c9fc3e235bb0f33a8daff4
bartekparysek <bartek.parysek@gmail.com>
2021-09-11 15:15:01 +0200

    deleted Redux

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import reduxThunk from "redux-thunk";
-
 import App from "./components/App";
-import reducers from "./reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-	reducers,
-	composeEnhancers(applyMiddleware(reduxThunk))
-);
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(
+// 	reducers,
+// 	composeEnhancers(applyMiddleware(reduxThunk))
+// );
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<App />,
 	document.querySelector("#root")
 );
 
-if (window.Cypress) {
-	window.store = store;
-}
+// if (window.Cypress) {
+// 	window.store = store;
+// }
