031292f7e629d9c83f3ee9eea4f3e8c2c8abd9c7
bartekparysek <bartek.parysek@gmail.com>
2021-10-10 12:23:15 +0200

    added default spinner

---
+++ src/components/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SpinnerCircularFixed } from 'spinners-react'
+
+const Spinner = () => {
+  return <SpinnerCircularFixed color="rgba(94, 114, 125, 0.55)" secondaryColor="rgba(255, 255, 255, 1)" />
+}
+
+export default Spinner
