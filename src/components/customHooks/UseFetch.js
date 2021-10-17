9ab99382ef72998b2652d96ffc9efb19014198c6
bartekparysek <bartek.parysek@gmail.com>
2021-10-17 12:15:52 +0200

    added custom useFetch hook

---
+++ src/components/customHooks/UseFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import spaceX from "../../apis/spaceX";
+
+const useFetch = (url) => {
+  const [data, setData] = useState({ data: [], isFetching: false, error: null });
+  const [endpoint] = useState(url);
+
+  useEffect(() => {
+    if (!endpoint) return;
+
+    const fetchData = async () => {
+      try {
+        setData((prev) => ({ ...prev, isFetching: true }));
+        const response = await spaceX.get(endpoint);
+        setData((prev) => ({
+          ...prev,
+          data: response.data,
+          isFetching: false
+        }));
+      } catch (error) {
+        setData((prev) => ({ ...prev, isFetching: false, error: error }))
+      }
+    }
+    fetchData();
+  }, [endpoint])
+  return data
+
+}
+export default useFetch;
