78499406339a813642c7eb30ae337e30731e289c
bartekparysek <bartek.parysek@gmail.com>
2021-10-29 17:37:21 +0200

    condition for link icon

--- src/components/FlipCard/DetailsLink.js
+++ src/components/FlipCard/DetailsLink.js
@@ -3,12 +3,12 @@ import { WrapperButton } from "./BackSide";
 import Link from "../Link";
 import { GrLinkNext } from "react-icons/gr";
 
-const DetailsLink = ({ flight }) => {
+const DetailsLink = ({ flight, dropdown }) => {
   return (
     <div>
       <Link to={`/flightdetails/${flight.id}`}>
         <WrapperButton aria-label={"Check Details"}>
-          <GrLinkNext />
+          {!dropdown && <GrLinkNext />}
           <p>CHECK DETAILS</p>
         </WrapperButton>
       </Link>
