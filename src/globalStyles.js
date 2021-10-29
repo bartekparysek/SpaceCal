cf53a9d08834e0273d1150a5b06be262fec96943
bartekparysek <bartek.parysek@gmail.com>
2021-10-29 19:16:20 +0200

    added linear gradient at main site, and background color for mobile

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,42 +1,40 @@
 import { createGlobalStyle } from "styled-components";
-import image from "./assets/app_background.jpeg";
-import mobile_image from "./assets/mobile_background.webp";
 
 const GlobalStyle = createGlobalStyle`
-   *{
-      box-sizing: border-box;
-   }
+  *{
+    box-sizing: border-box;
+  }
 
    html,body {
-      color: #000000;
-      font-family: 'News Cycle', sans-serif;
-      font-size: clamp(0.8rem,0.6vw + 0.6rem, 1rem);
-      margin:0;
-      height: 100%;
-   }
+    color: #000000;
+    font-family: 'News Cycle', sans-serif;
+    font-size: clamp(0.8rem,0.6vw + 0.6rem, 1rem);
+    margin:0;
+    height: 100%;
+  }
    body{
-      background-image: url(${image});
-      background-position: center center;
-	   background-size: cover;
-      background-repeat: no-repeat;
-      background-attachment: fixed;
-      @media (max-width: 425px){
-         background-image: url(${mobile_image});
-      }
+    background: -webkit-linear-gradient(255,233,184);
+    background: linear-gradient(0deg, rgba(255,233,184,1) 6%, rgba(167,188,223,1) 41%, rgba(25,80,172,1) 100%);
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    @media (max-width: 500px){
+      background: #1950ac;
+
+    }
    }
    h1{
-      font-size: clamp(1.75rem, 2rem, 2rem);
-      padding-left: 1rem;
-      white-space: nowrap;
-      @media (max-width: 375px){
-         margin:1rem;
-         padding-left:0;
-      }
+    font-size: clamp(1.75rem, 2rem, 2rem);
+    padding-left: 1rem;
+    white-space: nowrap;
+    @media (max-width: 375px){
+        margin:1rem;
+        padding-left:0;
+    }
    }
 
-   p{
-      margin:0;
-   }
+  p{
+    margin:0;
+  }
 `;
 
 export default GlobalStyle;
